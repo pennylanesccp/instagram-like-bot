be61b5af19b004e355fca333ee73f99cc008a04b
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-26 01:52:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ TARGET_USERNAME = input("Enter the target username: ")
 
 if not INSTAGRAM_USERNAME or not INSTAGRAM_PASSWORD or not TARGET_USERNAME:
     print("Please set the INSTAGRAM_USERNAME, INSTAGRAM PASSWORD and TARGET_USERNAME variables.")
+    exit()

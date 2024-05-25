3d71e048b91baad6564a2a204b54a337dc8872ac
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-26 01:50:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import time
 INSTAGRAM_USERNAME = ""
 INSTAGRAM_PASSWORD = ""
 TARGET_USERNAME = input("Enter the target username: ")
+
+if not INSTAGRAM_USERNAME or not INSTAGRAM_PASSWORD or not TARGET_USERNAME:
+    print()

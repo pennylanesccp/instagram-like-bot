bd9d1d36dbfb0d8f3e939b5d0c3386090791126a
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-05-26 01:54:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ TARGET_USERNAME = input("Enter the target username: ")
 if not INSTAGRAM_USERNAME or not INSTAGRAM_PASSWORD or not TARGET_USERNAME:
     print("Please set the INSTAGRAM_USERNAME, INSTAGRAM PASSWORD and TARGET_USERNAME variables.")
     exit()
+
+L = instaloader.Instaloader()

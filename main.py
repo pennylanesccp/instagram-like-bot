3c334c05b6affaba6b9d00e007f581020ae933e0
Felipe <felipeproenca97@gmail.com>
2024-09-23 15:00:28 -0300

    modifications to secure sensitive data + gitignore

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import instaloader
 import requests
 import time
 
-INSTAGRAM_USERNAME = ""
-INSTAGRAM_PASSWORD = ""  
-TARGET_USERNAME = input("Enter the target username: ")
-
-if not INSTAGRAM_USERNAME or not INSTAGRAM_PASSWORD or not TARGET_USERNAME:
-    print("Please set the INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, and TARGET_USERNAME variables.")
-    exit()
-
-L = instaloader.Instaloader()
-L.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
-profile = instaloader.Profile.from_username(L.context, TARGET_USERNAME)
-session = L.context._session
-
 def like_post(post):
     post_url = f"https://www.instagram.com/web/likes/{post.mediaid}/like/"
     response = session.post(post_url)
@@ -23,8 +10,23 @@ def like_post(post):
     else:
         print(f"Failed to like post: {post.shortcode}, Status code: {response.status_code}")
 
-for post in profile.get_posts():
-    like_post(post)
-    time.sleep(30)
+def like_bot(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, TARGET_USERNAME, n=0):
+    if not INSTAGRAM_USERNAME or not INSTAGRAM_PASSWORD or not TARGET_USERNAME:
+        print("Please set the INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, and TARGET_USERNAME variables.")
+        exit()
 
-print(f"Liked all posts of user: {TARGET_USERNAME}")
+    L = instaloader.Instaloader()
+    L.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+    profile = instaloader.Profile.from_username(L.context, TARGET_USERNAME)
+    session = L.context._session
+
+    if n == 0:
+        posts = profile.get_posts()
+    else:
+        posts = profile.get_posts()[0:n]
+
+    for post in posts:
+        like_post(post)
+        time.sleep(30)
+
+    print(f"Liked all posts of user: {TARGET_USERNAME}")

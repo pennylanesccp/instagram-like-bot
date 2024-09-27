09175c3a29036573c33f0b8cd3a6a66731eed1b4
Felipe <felipeproenca97@gmail.com>
2024-09-27 13:46:17 -0300

    executavel final

--- executable.py
+++ executable.py
@@ -5,10 +5,10 @@ INSTAGRAM_USERNAME = "felipesaproenca"
 INSTAGRAM_PASSWORD = main()
 TARGET_USERNAME_LIST = [
     "patiza1609"
-    # , "isagumierolee"
-    # , "1casamentoperfeito"
+    , "isagumierolee"
+    , "1casamentoperfeito"
 ]
-n=5
+n=0
 
 for TARGET_USERNAME in TARGET_USERNAME_LIST:
     like_bot(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD, TARGET_USERNAME, n)
